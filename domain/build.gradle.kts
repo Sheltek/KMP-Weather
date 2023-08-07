c4f8e69fac91b2854600cc0446d5172553b869d8
Vanya Huaman <vanya.huaman@bottlerocketstudios.com>
2023-08-07 10:48:34 -0500

    Add koin

---
+++ domain/build.gradle.kts
@@ -0,0 +1,68 @@
+plugins {
+    kotlin("multiplatform")
+    id("com.android.library")
+    kotlin("plugin.serialization") version "1.8.21"
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+val ktorVersion = "2.3.2"
+val coroutineCoreVersion = "1.7.3"
+val koinVersion = "3.2.0"
+val koinComposeVersion = "3.4.2"
+
+@OptIn(org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi::class)
+kotlin {
+
+    android {
+        kotlinOptions {
+            jvmTarget = "1.8"
+        }
+    }
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                //put your multiplatform dependencies here
+
+                implementation("io.ktor:ktor-client-core:$ktorVersion")
+                implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
+                implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
+                implementation("io.insert-koin:koin-core:${koinVersion}")
+            }
+        }
+        val androidMain by getting {
+            dependencies {
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutineCoreVersion")
+                implementation("io.ktor:ktor-client-android:$ktorVersion")
+                implementation("io.insert-koin:koin-android:${koinVersion}")
+                implementation("io.insert-koin:koin-androidx-compose:${koinComposeVersion}")
+            }
+        }
+        val iosMain by getting {
+            // ...
+            dependencies {
+                implementation("io.ktor:ktor-client-darwin:$ktorVersion")
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+                implementation("io.insert-koin:koin-test:${koinVersion}")
+            }
+        }
+    }
+}
+
+android {
+    namespace = "com.br.kmmdemo"
+    compileSdk = 33
+    defaultConfig {
+        minSdk = 30
+    }
+}
--- shared/src/iosMain/kotlin/com/br/kmmdemo/Helper.kt
+++ shared/src/iosMain/kotlin/com/br/kmmdemo/Helper.kt
@@ -1,7 +1,6 @@
 package com.br.kmmdemo
 
 import com.br.kmmdemo.di.appModule
-import com.br.kmmdemo.di.commonModule
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 import org.koin.core.context.startKoin
