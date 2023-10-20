083d89c61317ef19386dbc6f6213de397da972d9
Dior DeLeon <dior.deleon@bottlerocketstudios.com>
2023-10-20 13:16:36 -0400

    Weather Map

--- shared/src/androidMain/kotlin/com/br/kmmdemo/main/android.kt
+++ shared/src/androidMain/kotlin/com/br/kmmdemo/main/android.kt
@@ -2,6 +2,10 @@ package com.br.kmmdemo.main
 
 import androidx.compose.runtime.Composable
 import com.br.kmmdemo.App
+import com.br.kmmdemo.WeatherMapScreen
 
 @Composable
 fun MainView() = App()
+
+@Composable
+fun WeatherMapView() = WeatherMapScreen()
