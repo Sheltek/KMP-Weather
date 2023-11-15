a4d137348fa8ceee797264bbb33ac51f10317d57
Vanya Huaman <VHuaman@caesars.com>
2023-11-15 15:12:58 -0600

    Add logger

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -62,6 +62,10 @@ kotlin {
                 implementation(libs.ktor.client.content.negotiation)
                 implementation(libs.ktor.serialization.kotlinx.json)
                 implementation(libs.koin.core)
+                implementation(libs.kermit.logger)
+
+                // Logger
+//                implementation(libs.)
             }
         }
         val androidMain by getting {
