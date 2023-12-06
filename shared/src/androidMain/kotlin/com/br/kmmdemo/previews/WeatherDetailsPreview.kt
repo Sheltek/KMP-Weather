9315da7a1476cd857cda1ac56730ae3c9080924e
Micheal Murray <michealmurray16@gmail.com>
2023-12-06 15:14:08 -0600

    [ASAA-137] Weather Details: Create Generic Informational
    - Added necessary strings for new components and update some strings to be used in mulitple locations
    - Created new state objects `FeelsLikeState,` `HumidityState,` `RainFallState,` and `VisibilityState`
    - Create new widgets `FeelsLikeWidget,` `HumidityWidget,` `RainFallWidget,` and `VisibilityWidget`

--- shared/src/androidMain/kotlin/com/br/kmmdemo/previews/WeatherDetailsPreview.kt
+++ shared/src/androidMain/kotlin/com/br/kmmdemo/previews/WeatherDetailsPreview.kt
@@ -1,6 +1,7 @@
 package com.br.kmmdemo.previews
 
 import FeelsLikeWidget
+import VisibilityWidget
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.runtime.Composable
@@ -9,8 +10,13 @@ import com.br.kmmdemo.theme.KMMTheme
 import com.br.kmmdemo.ui.weatherDetails.airQuality.AirQualityEnum
 import com.br.kmmdemo.ui.weatherDetails.airQuality.AirQualityWidget
 import com.br.kmmdemo.ui.weatherDetails.feelsLike.FeelsLikeState
+import com.br.kmmdemo.ui.weatherDetails.humidity.HumidityState
+import com.br.kmmdemo.ui.weatherDetails.humidity.HumidityWidget
+import com.br.kmmdemo.ui.weatherDetails.rainFall.RainFallState
+import com.br.kmmdemo.ui.weatherDetails.rainFall.RainFallWidget
 import com.br.kmmdemo.ui.weatherDetails.uvIndex.UVIndexEnum
 import com.br.kmmdemo.ui.weatherDetails.uvIndex.UvIndexWidget
+import com.br.kmmdemo.ui.weatherDetails.visibility.VisibilityState
 
 @Composable
 @Preview
@@ -42,6 +48,39 @@ fun `Feels Like Preview`() {
         Row {
             FeelsLikeWidget(FeelsLikeState(75.0, 65.0))
             FeelsLikeWidget(FeelsLikeState(null, 59.0))
+        }
+    }
+}
+
+@Composable
+@Preview
+fun `Humidity Preview`() {
+    KMMTheme {
+        Row {
+            HumidityWidget(HumidityState(90.0, 17.0))
+            HumidityWidget(HumidityState(null, null))
+        }
+    }
+}
+
+@Composable
+@Preview
+fun `Rain Fall Preview`() {
+    KMMTheme {
+        Row {
+            RainFallWidget(RainFallState("1.8 mm", "1.2 mm"))
+            RainFallWidget(RainFallState())
+        }
+    }
+}
+
+@Composable
+@Preview
+fun `Visibility Preview`() {
+    KMMTheme {
+        Row {
+            VisibilityWidget(VisibilityState("8 km", true))
+            VisibilityWidget(VisibilityState())
         }
     }
 }
